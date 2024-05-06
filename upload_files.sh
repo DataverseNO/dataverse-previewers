0b6ff0b0ea9d0f1cae999546d2622d9065ad6c48
Benedikt Meier <benedikt.meier@uit.no>
2024-05-06 17:55:55 +0200

    betatest change url and create script to download

---
+++ upload_files.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+dirLocal=$(pwd)
+
+# go to the version folder
+if [ -z "$1" ]; then
+    echo "The variable is empty."
+fi
+folder="$1"
+
+cd "${folder}"
+if [ ! -d ./css ]; then
+  mkdir ./css
+fi
+cd ./css
+while read url; do
+    wget --quiet $url
+done < "${dirLocal}/urls_css.txt"
+
+cd "${folder}"
+if [ ! -d ./js ]; then
+  mkdir ./js
+fi
+cd ./js
+while read url; do
+    wget --quiet $url
+done < "${dirLocal}/urls_js.txt"
