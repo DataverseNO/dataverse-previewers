3f8c59fbfe6bebff35509e22b7650fcfc5424ba5
Benedikt Kruse <benedikt.kruse@uit.no>
2025-04-28 11:39:56 +0200

    add init version 1.5

--- docker/scripts/init.sh
+++ docker/scripts/init.sh
@@ -3,7 +3,7 @@
 dirLocal=$(pwd)
 
 # Set some defaults as documented
-VERSIONS=${VERSIONS:-"v1.4"}
+VERSIONS=${VERSIONS:-"v1.5"}
 
 echo "Using Provider Url=${PREVIEWERS_PROVIDER_URL} for versions ${VERSIONS}"
 cd /app
@@ -14,6 +14,11 @@ fi
 if echo "$VERSIONS" | grep -q "\bv1.4\b"; then
     ./localinstall.sh previewers/v1.4 ${PREVIEWERS_PROVIDER_URL}
     cp *.md previewers/v1.4
+fi
+fi
+if echo "$VERSIONS" | grep -q "\bv1.5\b"; then
+    ./localinstall.sh previewers/v1.5 ${PREVIEWERS_PROVIDER_URL}
+    cp *.md previewers/v1.5
 fi
 if echo "$VERSIONS" | grep -q "\bbetatest\b"; then
     ./localinstall.sh previewers/betatest ${PREVIEWERS_PROVIDER_URL}
